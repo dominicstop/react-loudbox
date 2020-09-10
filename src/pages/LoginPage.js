1ddb52fd56c5afad0c78a21cb0596fc9ce88befc
Dominic Go <dominicstop@gmail.com>
2020-09-10 15:20:31 +0800

    ✨ WIP: LoginPage
    Summary: Added bg image

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 
+
+
 export class LoginPage extends React.Component {
   static styles = StyleSheet.create({
     rootContainer: {
@@ -12,29 +14,37 @@ export class LoginPage extends React.Component {
     leftFormContainer: {
       flex: 1,
       display: 'flex',
-      backgroundColor: 'red',
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     rightImageContainer: {
-      flex: 1,
-      display: 'flex',
-      backgroundColor: 'blue',
-      '@media (max-width: 1000px)': {
-        display: 'none'
+      display: 'none',
+      // set/config bg image
+      backgroundImage: `url(${require('assests/images/login-cover.jpg')})`,
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      // show the right image container if there's enough space
+      '@media (min-width: 750px) and (max-width: 1250px)': {
+        flex: 1,
+        display: 'flex'
+      },
+      '@media (min-width: 1250px)': {
+        flex: 2,
+        display: 'flex'
       }
     },
   });
 
   render(){
     const { styles } = LoginPage;
-
     return(
       <div className={css(styles.rootContainer)}>
         <div className={css(styles.leftFormContainer)}>
           <h1>Hello</h1>
         </div>
-        <div className={css(styles.rightImageContainer)}>
-          <h1>Hello World</h1>
-        </div>
+        <div className={css(styles.rightImageContainer)}/>
       </div>
     );
   };
