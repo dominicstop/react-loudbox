e4c7949b0ce5319afe504232867f06985af52cad
Dominic Go <dominicstop@gmail.com>
2020-09-11 06:16:57 +0800

    ✨ WIP: LoginPage - Login Form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
+import { Typography, Box } from '@material-ui/core';
+import { FormControl, InputLabel, InputBaseProps, FormHelperText } from '@material-ui/core';
+
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
+
 import loginBG     from 'assests/images/login-cover.jpg';
 import loudboxLogo from 'assests/svg/loudbox-logo.svg';
 
@@ -19,12 +24,23 @@ export class LoginPage extends React.Component {
       alignItems: 'center',
       justifyContent: 'center',
     },
+    formContainer: {
+      height: 'auto',
+      width: '320px',
+      paddingTop: '64px',
+      paddingBottom: '64px',
+      textAlign: 'center',
+    },
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
     rightImageContainer: {
       display: 'none',
       // set/config bg image
       backgroundImage: `url(${loginBG})`,
       backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
+      backgroundRepeat: 'norepeat',
       backgroundSize: 'cover',
       // show the right image container if there's enough space
       '@media (min-width: 750px) and (max-width: 1250px)': {
@@ -43,18 +59,41 @@ export class LoginPage extends React.Component {
     },
   });
 
+  _handleOnFormSubmit = () => {
+
+  };
+
   render(){
     const { styles } = LoginPage;
     return(
       <div className={css(styles.rootContainer)}>
         <div className={css(styles.leftFormContainer)}>
-          <div>
+          <Box className={css(styles.formContainer)}>
+            <Typography
+              variant="h5"
+              component="h1"
+            >
+              Welcome to
+            </Typography>
             <img 
               className={css(styles.logo)}
               src={loudboxLogo} 
               alt="LoudBox Logo" 
             />
-          </div>
+            <Formik>
+              {(params) => (
+                <Form className={css(styles.form)}>
+                  <Field type="email" name="email" />
+                  <ErrorMessage name="email" component="div" />
+                  <Field type="password" name="password" />
+                  <ErrorMessage name="password" component="div" />
+                  <button type="submit" disabled={params.isSubmitting}>
+                    Submit
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          </Box>
         </div>
         <div className={css(styles.rightImageContainer)}/>
       </div>
