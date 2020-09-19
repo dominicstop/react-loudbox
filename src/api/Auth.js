e516056982ff4af2ef170cfa46a0aa204cc06fd2
Dominic Go <dominicstop@gmail.com>
2020-09-19 18:39:40 +0800

    ✨ WIP: Login API
    1) Added: Endpoints.,
    2) Added: Auth - initial impl. for AuthLogin.,

---
+++ src/api/Auth.js
@@ -0,0 +1,136 @@
+
+import * as Endpoints from 'constants/Endpoints';
+
+import { LoginPayload  } from 'models/LoginPayload';
+import { ErrorModel    } from 'models/ErrorModel';
+import { LoginResponse, LoginResponseKeys } from 'models/LoginResponse';
+
+export const testLoginCredentials = LoginPayload.factory({
+  username: 'admin@gmail.com',
+  password: 'adminuser'
+});
+
+
+export class AuthLogin {
+  /** possible error types based on the login response
+   * @readonly
+   * @enum {string}
+   */
+  static ERROR_TYPES = {
+    CredentialsError  : 'CredentialsError'  ,
+    EmptyEmail        : 'EmptyEmail'        ,
+    EmptyPassword     : 'EmptyPassword'     ,
+    EmptyEmailPassword: 'EmptyEmailPassword',
+    UnknownError      : 'UnknownError'      ,
+  };
+
+  /** get the corresonding 'friendly' error message for errorType */
+  static getErrorMessage(errorType){
+    const { ERROR_TYPES } = AuthLogin;
+
+    switch (errorType) {
+      case ERROR_TYPES.CredentialsError  : return 'Invalid Email or Password';
+      case ERROR_TYPES.EmptyEmail        : return 'Email cannot be empty';
+      case ERROR_TYPES.EmptyPassword     : return 'Password cannot be empty';
+      case ERROR_TYPES.EmptyEmailPassword: return 'Email and Password cannot be empty';
+        
+      case ERROR_TYPES.UnknownError:
+      default: return 'Looks like something went wrong';
+    };
+  };
+
+  /** get the error type based on the login response */
+  static getErrorTypeFromResponse(response = {}){
+    const { ERROR_TYPES } = AuthLogin;
+
+    const isCredentialsError = (
+      ('non_field_errors' in response             ) && 
+      (Array.isArray(response['non_field_errors'])) &&
+      (response['non_field_errors']?.[0] == 'Incorrect Credentials')
+    );
+
+    const isEmptyUsernameError = (
+      ('username' in response          ) && 
+      (Array.isArray(response.username)) &&
+      (response.username?.[0] == 'This field may not be blank.') ||
+      (response.username?.[0] == 'This field may not be null.' ) 
+    );
+
+    const isEmptyPasswordError = (
+      ('password' in response          ) && 
+      (Array.isArray(response.password)) &&
+      (response.password?.[0] == 'This field may not be blank.') ||
+      (response.password?.[0] == 'This field may not be null.' ) 
+    );
+
+    const isEmptyUsernamePassword = 
+      (isEmptyUsernameError && isEmptyPasswordError);
+
+    return (
+      isCredentialsError     ? ERROR_TYPES.CredentialsError   :
+      isEmptyUsernamePassword? ERROR_TYPES.EmptyEmailPassword :
+      isEmptyUsernameError   ? ERROR_TYPES.EmptyEmail         :
+      isEmptyPasswordError   ? ERROR_TYPES.EmptyPassword      : ERROR_TYPES.UnknownError
+    );
+  };
+
+  /** checks whether the login was successful based on the response */
+  static isLoginSuccess(response = {}){
+    return (
+      (LoginResponseKeys.user           in response) &&
+      (LoginResponseKeys.token          in response) &&
+      (LoginResponseKeys.expirationDate in response) 
+    );
+  };
+
+  static async login(payload = LoginPayload.structure){
+    const loginPayload = LoginPayload.factory(payload);
+    
+    try {
+      const response = await fetch(Endpoints.loginURL, {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json',  },
+        body: JSON.stringify(loginPayload)
+      });
+
+      // parse response and check if login success
+      const json      = await response.json();
+      const isSuccess = AuthLogin.isLoginSuccess(json);
+
+      if(isSuccess){
+        const loginResponse = LoginResponse.factory(json);
+        alert('login success');
+
+      } else {
+        const errorType    = AuthLogin.getErrorTypeFromResponse(json);
+        const errorMessage = AuthLogin.getErrorMessage(errorType);
+
+        const error = ErrorModel.factory({
+          error       : JSON.stringify(json),
+          errorKey    : errorType,
+          errorMessage: errorMessage,
+        });
+
+        alert(`login failed ${errorType}`);
+        return { isSuccess: false, error };
+      };
+      
+
+      console.log('login response: ', json);
+      console.log('login response keys: ', Object.keys(json));
+
+
+    } catch(error){
+      const { ERROR_TYPES } = AuthLogin;
+
+      const errorObj = ErrorModel.factory({
+        error       : JSON.stringify(error),
+        errorKey    : ERROR_TYPES.UnknownError,
+        errorMessage: 'An unexpected error has occured',
+      });
+
+      return { isSuccess: false, error: errorObj };
+    };
+  };
+};
---
+++ src/constants/Endpoints.js
@@ -0,0 +1,8 @@
+
+export const baseURL = 'https://abstract-user-dot-heroic-climber-277222.df.r.appspot.com';
+
+export const API_PATHS = {
+  login: 'login'
+};
+
+export const loginURL = `${baseURL}/${API_PATHS.login}/`;
