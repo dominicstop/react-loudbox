5ffe6a13542f591c85451c6286ca551254f4c277
Dominic Go <dominicstop@gmail.com>
2020-09-27 21:05:26 +0800

    🤏 Minor Changes: HomePage
    Summary: Re-Added temp. logout button to `HomePage `

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import AuthStore from 'functions/AuthStore';
 
 
 export default function HomePage(props){
   return(
     <div>
       <h1>Home</h1>
+      <button onClick={() => {
+        AuthStore.resetAuth();
+        props.history.push('/login');
+      }}>
+        {'Log Out'}
+      </button>
     </div>
   );
 };
