c45d0b6b96fb44f4c352d07a8012f36a418f19e2
Dominic Go <dominicstop@gmail.com>
2020-09-30 06:37:38 +0800

    ✨ WIP: AdminFileManagePage
    Summary: duumy impl. to use FileManagement API

--- src/pages/AdminFileManagePage.js
+++ src/pages/AdminFileManagePage.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { withAuthRedirect } from 'hoc/withAuthRedirect';
+import { FileManagement } from 'api/FileManagement';
+import * as Helpers from 'functions/helpers';
+import { withFetchAPI } from 'hoc/withFetchAPI';
 
 
-function AdminFileManagePage(props){
-  return(
-    <div>
-      <h1>AdminFileManagePage</h1>
-    </div>
-  );
+const LOADING_MODE = {
+  IDLE   : 'IDLE'   ,
+  LOADING: 'LOADING',
+  ERROR  : 'ERROR'  ,
 };
 
-export default withAuthRedirect(AdminFileManagePage, 'OnlyAdmin');
+class AdminFileManagePage extends React.Component {
+  render(){
+    const { fetchData, fetchTimestamp, fetchLoadingMode } = this.props;
+    return(
+      <div>
+        <h1>AdminFileManagePage</h1>
+        <p>{`fetchData count : ${fetchData?.length}`}</p>
+        <p>{`fetchLoadingMode: ${fetchLoadingMode }`}</p>
+        <p>{`fetchTimestamp  : ${fetchTimestamp   }`}</p>
+        <button onClick={() => {
+          this.props.clearFetchData();
+        }}>
+          clear
+        </button>
+      </div>
+    );
+  };
+};
+
+const pageWithAPI = withFetchAPI(AdminFileManagePage, {
+  fetchAPI: FileManagement
+});
+
+export default withAuthRedirect(pageWithAPI, 'OnlyAdmin');
